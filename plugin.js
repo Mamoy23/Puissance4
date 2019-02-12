76f72d13febcd419eb76d57255ebb5e30503a9eb
Marine <marine.moynet@epitech.eu>
2019-02-12 10:51:15 +0100

    Check victoires horizontal + vertical

--- plugin.js
+++ plugin.js
@@ -1,47 +1,186 @@
 function createGrid(x, y) {
-  for (var columns = 0; columns < x; columns++) {
-    for (var rows = 0; rows < y; rows++) {
+  for (let columns = 0; columns < x; columns++) {
+    for (let rows = 0; rows < y; rows++) {
       $("#grid").append("<div class='subgrid'></div>");
     };
   };
-  //var width = $("#grid").width() + ($(".subgrid").css("margin"));
-  //var width = $("#grid").outerWidth(true);
-  //var height = $("#grid").outerHeight(true);
+  //let width = $("#grid").width() + ($(".subgrid").css("margin"));
+  //let width = $("#grid").outerWidth(true);
+  //let height = $("#grid").outerHeight(true);
   //console.log(width +' '+height);
   //console.log($("#grid").height());
   $(".subgrid").width($("#grid").width()/x);
   $(".subgrid").height($("#grid").height()/y);
 };
 
+let array = [];
+let turn = 0;
+
 function createGrid2(x, y){
-  var array = [];
-  $("#grid").append('<table></table>');
-  for (var rows = 0; rows < y; rows++) {
+  $('body').prepend('<div>');
+  $('div').attr('id', 'grid');
+  $("#grid").append('<table>');
+  for (let rows = 0; rows < y; rows++) {
     array[rows] = [];
-    $("table").append("<tr></tr>");
-    for (var columns = 0; columns < x; columns++) {
+    let tr = document.createElement('tr');
+    for (let columns = 0; columns < x; columns++) {
+      let td = document.createElement('td');
+      td.setAttribute('id', rows+'-'+columns);
+      tr.append(td);
       array[rows][columns] = 0;
     }
+    $('table').append(tr); 
   }
-  for (var columns = 0; columns < x; columns++) {
-    $("tr").append("<td></td>");
-  }
-  console.log(array);
+  //for (let columns = 0; columns < x; columns++) {
+    //$("tr").append("<td id='"+rows+"-"+columns+"'>");
+  //}
+  $('body').prepend('<div id="bar">');
+  $('#bar').css({'color':'blue', 'text-align':'center'});
 }
 
-$(document).ready(function(){
-  createGrid2(6, 6);
-  var count = 0;
+
+function getClickPosition(){
+
   $("td").click(function(){
-    console.log(count);
-    if(!$(this).hasClass("player1") && !$(this).hasClass("player2")){
-      count += 1;
-      if(count%2 == 0){
-        $(this).addClass('player1');
-      };
-      if(count%2 == 1){
-        $(this).addClass('player2');
-      };
-    };
-  });
+    let column = $(this).index();
+    let len = array.length - 1;
+    let row;
+    turn += 1;
+    for (row = len; row > -1; row--) {
+      if (array[row][column] == 0) {
+        if(turn%2 == 1){
+        array[row][column] = 1;
+        $(`#${row}-${column}`).addClass('player1');
+        }
+        if(turn%2 == 0){
+          array[row][column] = 2;
+          $(`#${row}-${column}`).addClass('player2');
+        }
+        break;
+      }
+    }
+    if(row < 0){
+      alert('Cette colonne est pleine!');
+    }
+    let joueur = $(`#${row}-${column}`).attr('class');
+
+    if(joueur == 'player1'){
+      $('#bar').text('Tour de : player2');
+    }
+    if(joueur == 'player2'){
+      $('#bar').text('Tour de : player1');
+    }
+    let player = array[row][column];
+    let count = 0;
+    let win = 0;
+    let maxRow = array.length;
+    let maxCol = array[row].length;
+
+    //CHECK HORIZONTAL
+    for(i = 0; i < maxCol; i++){
+      if(array[row][i] == player){
+        count++;
+        if(count >= 4){
+          alert(player+' win ! END OF THE GAME');
+          $('td').removeClass();
+          $.each(array, function(row, column){
+            $.each(column, function(key, value){
+              column[key]= 0;
+            });
+          });
+        }
+      }
+      else{
+        count = 0;
+      }
+    }
+    count = 0;
+    //CHECK VERTICAL
+    for(i = 0; i < maxRow; i++){
+      if(array[i][column] == player){
+        count++;
+        if(count >= 4){
+          alert(player+' win ! END OF THE GAME');
+          $('td').removeClass();
+          $.each(array, function(row, column){
+            $.each(column, function(key, value){
+              column[key]= 0;
+            });
+          });
+          return 1;
+        }
+      }
+      else{
+        count = 0;
+      }
+    }
+
+
+    // //CHECK DIAGONALE BAS DROIT VERT HAUT GAUCHE OK
+    // for(i = row+1, j=column+1; i<maxRow && j<maxCol; i++, j++){
+    //   if(array[i][j] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+    // count = 0;
+
+    // for(i = row-1, j=column-1; i>= 0 && j>= 0; i--, j--){
+    //   if(array[i][j] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+
+
+    //DIAGONAL BAS GAUCHE VERS HAUT DROIT
+    // for(i= column-1, j= row-1; i>=0, j>=0; i--, j--){
+    //   if(array[j][i] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+    // let rowIndex = $(this).parent().index();
+    // let columnIndex = $(this).index();
+    // console.log(array);
+    // for(let i = columnIndex; i >= 0; i--){
+      //   console.log(array[rowIndex][i]);
+      //   if(array[rowIndex][i] == 0){
+           //if(!$(this).hasClass("player1") && !$(this).hasClass("player2")){
+          //       turn += 1;
+          //       if(turn%2 == 0){
+            //         array[rowIndex][i] = 1;
+            //         $(this).addClass('player1');
+            //       };
+            //       if(turn%2 == 1){
+              //         array[rowIndex][i] = 2;
+              //         $(this).addClass('player2');
+              //       };
+              //     };
+              //   }
+              // }
+    });
+};
+
+$(document).ready(function(){
+  createGrid2(7, 6);
+  getClickPosition();
 });
