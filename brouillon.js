521663c15c5546a372f09e9bf2cbb05d20cf0468
Marine <marine.moynet@epitech.eu>
2019-02-15 08:37:38 +0100

    Check win diago + turn + undo last move + score + options plugins

--- brouillon.js
+++ brouillon.js
@@ -1,31 +1,244 @@
-  var array = [];
+function createGrid(x, y) {
+  for (let columns = 0; columns < x; columns++) {
+    for (let rows = 0; rows < y; rows++) {
+      $("#grid").append("<div class='subgrid'></div>");
+    };
+  };
+  //let width = $("#grid").width() + ($(".subgrid").css("margin"));
+  //let width = $("#grid").outerWidth(true);
+  //let height = $("#grid").outerHeight(true);
+  //console.log(width +' '+height);
+  //console.log($("#grid").height());
+  $(".subgrid").width($("#grid").width()/x);
+  $(".subgrid").height($("#grid").height()/y);
+};
+
+let array = [];
+let turn = 0;
+
+function createGrid2(x, y){
   $('body').prepend('<div>');
   $('div').attr('id', 'grid');
   $("#grid").append('<table>');
-  for (var rows = 0; rows < y; rows++) {
+  for (let rows = 0; rows < y; rows++) {
     array[rows] = [];
-    $("table").append("<tr>");
-    for (var columns = 0; columns < x; columns++) {
+    let tr = document.createElement('tr');
+    for (let columns = 0; columns < x; columns++) {
+      let td = document.createElement('td');
+      td.setAttribute('id', rows+'-'+columns);
+      tr.append(td);
       array[rows][columns] = 0;
     }
+    $('table').append(tr); 
   }
-  for (var columns = 0; columns < x; columns++) {
-    $("tr").aparraypend("<td id='"+rows+"-"+columns+"'>");
-  }
+  //for (let columns = 0; columns < x; columns++) {
+    //$("tr").append("<td id='"+rows+"-"+columns+"'>");
+  //}
+  $('body').prepend('<div id="bar">');
+  $('#bar').css({'color':'blue', 'text-align':'center'});
+}
 
-  });
 
-          // let textarea = $("#textarea");
-        // let start = textarea[0].selectionStart;
-        // console.log(start);
-        // let finish = textarea[0].selectionEnd;
-        // console.log(finish);
-        // let sel = textarea.val();
-        // let lol = sel.substring(start, finish);
-        // console.log(lol);
-        // let underline = $(this).css('text-decoration', 'underline');
-        // $(lol).html(underline);
-        //document.$(lol).execCommand('underline');
-        //$(lol).wrapInner('<span />');
-        //$(lol).html('<span style="text-decoration: underline;">')
-        //$(lol).addClass('underline');
+function play(){
+
+  $("td").click(function(){
+    let column = $(this).index();
+    let len = array.length - 1;
+    let row;
+    turn += 1;
+    for (row = len; row > -1; row--) {
+      if (array[row][column] == 0) {
+        if(turn%2 == 1){
+        array[row][column] = 1;
+        $(`#${row}-${column}`).addClass('player1');
+        }
+        if(turn%2 == 0){
+          array[row][column] = 2;
+          $(`#${row}-${column}`).addClass('player2');
+        }
+        break;
+      }
+    }
+    if(row < 0){
+      alert('Cette colonne est pleine!');
+    }
+    let joueur = $(`#${row}-${column}`).attr('class');
+
+    if(joueur == 'player1'){
+      $('#bar').text('Tour de : player2');
+    }
+    if(joueur == 'player2'){
+      $('#bar').text('Tour de : player1');
+    }
+    let player = array[row][column];
+    let count = 0;
+    let win = 0;
+    let maxRow = array.length;
+    let maxCol = array[row].length;
+
+    //CHECK HORIZONTAL
+    for(i = 0; i < maxCol; i++){
+      if(array[row][i] == player){
+        count++;
+        if(count >= 4){
+          alert(player+' win ! END OF THE GAME');
+          $('td').removeClass();
+          $.each(array, function(row, column){
+            $.each(column, function(key, value){
+              column[key]= 0;
+            });
+          });
+        }
+      }
+      else{
+        count = 0;
+      }
+    }
+  
+    //CHECK VERTICAL
+    for(i = 0; i < maxRow; i++){
+      if(array[i][column] == player){
+        count++;
+        if(count >= 4){
+          alert(player+' win ! END OF THE GAME');
+          $('td').removeClass();
+          $.each(array, function(row, column){
+            $.each(column, function(key, value){
+              column[key]= 0;
+            });
+          });
+        }
+      }
+      else{
+        count = 0;
+      }
+    }
+    //CHECK HAUT GAUCHE VERS BAS DROIT / PARTIE GAUCHE DU PLATEAU OK
+    // for( rowStart = 0; rowStart < maxRow - 4; rowStart++){
+    //   count = 0;
+    //   for( row = rowStart, col = 0; row < maxRow && col < maxCol; row++, col++ ){
+    //       if(array[row][col] == player){
+    //         count++;
+    //         if(count >= 4){
+    //           alert(player+' win ! END OF THE GAME');
+    //           $('td').removeClass();
+    //           $.each(array, function(row, column){
+    //             $.each(column, function(key, value){
+    //               column[key]= 0;
+    //             });
+    //           });
+    //         }
+    //       }
+    //       else {
+    //           count = 0;
+    //       }
+    //   }
+    // }
+    //CHECK HAUT GAUCHE VERS BAS DROIT / PARTIE DROITE DU PLATEAU OK
+    // for( colStart = 1; colStart < maxCol - 4; colStart++){
+    //   count = 0;
+    //   for( row = 0, col = colStart; row < maxRow && col < maxCol; row++, col++ ){
+    //       if(array[row][col] == player){
+    //           count++;
+    //           if(count >= 4){
+    //             alert(player+' win ! END OF THE GAME');
+    //             $('td').removeClass();
+    //             $.each(array, function(row, column){
+    //               $.each(column, function(key, value){
+    //                 column[key]= 0;
+    //               });
+    //             });
+    //           }
+    //       }
+    //       else {
+    //           count = 0;
+    //       }
+    //   }
+    // }
+    //BAS GAUCHE VERS HAUT DROIT OK
+    for (i=0; i<maxRow; i++){
+      for (j=0; j<maxCol; j++){
+          if (array[i][j] == player && array[i-1][j+1] == player && array[i-2][j+2] == player && array[i-3][j+3] == player){            
+            console.log('premiere boucle');
+            alert(player+' win ! END OF THE GAME');
+          }
+      }
+    }
+
+    //BAS DROIT VERS HAUT GAUCHE OK
+    for (i=0; i<maxRow; i++){
+      for (j=0; j<maxCol; j++){
+          if (array[i][j] == player && array[i-1][j-1] == player && array[i-2][j-2] == player && array[i-3][j-3] == player){
+            console.log('deuxieme boucle');
+            alert(player+' win ! END OF THE GAME');
+          }
+      }
+    }
+    // //CHECK DIAGONALE BAS DROIT VERT HAUT GAUCHE OK
+    // for(i = row+1, j=column+1; i<maxRow && j<maxCol; i++, j++){
+    //   if(array[i][j] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+    // count = 0;
+
+    // for(i = row-1, j=column-1; i>= 0 && j>= 0; i--, j--){
+    //   if(array[i][j] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+
+
+    //DIAGONAL BAS GAUCHE VERS HAUT DROIT
+    // for(i= column-1, j= row-1; i>=0, j>=0; i--, j--){
+    //   if(array[j][i] == player){
+    //     count++;
+    //     if(count >= 4){
+    //       alert(player+' win ! END OF THE GAME');
+    //       return 1;
+    //     }
+    //   }
+    //   else{
+    //     count = 0;
+    //   }
+    // }
+    // let rowIndex = $(this).parent().index();
+    // let columnIndex = $(this).index();
+    // console.log(array);
+    // for(let i = columnIndex; i >= 0; i--){
+      //   console.log(array[rowIndex][i]);
+      //   if(array[rowIndex][i] == 0){
+           //if(!$(this).hasClass("player1") && !$(this).hasClass("player2")){
+          //       turn += 1;
+          //       if(turn%2 == 0){
+            //         array[rowIndex][i] = 1;
+            //         $(this).addClass('player1');
+            //       };
+            //       if(turn%2 == 1){
+              //         array[rowIndex][i] = 2;
+              //         $(this).addClass('player2');
+              //       };
+              //     };
+              //   }
+              // }
+    });
+};
+
+$(document).ready(function(){
+  createGrid2(7, 6);
+  play();
+});
--- plugin.js
+++ plugin.js
@@ -1,186 +1,238 @@
-function createGrid(x, y) {
-  for (let columns = 0; columns < x; columns++) {
-    for (let rows = 0; rows < y; rows++) {
-      $("#grid").append("<div class='subgrid'></div>");
-    };
-  };
-  //let width = $("#grid").width() + ($(".subgrid").css("margin"));
-  //let width = $("#grid").outerWidth(true);
-  //let height = $("#grid").outerHeight(true);
-  //console.log(width +' '+height);
-  //console.log($("#grid").height());
-  $(".subgrid").width($("#grid").width()/x);
-  $(".subgrid").height($("#grid").height()/y);
-};
+(function($){
+  $.fn.power4 = function(options){
+      
+      var settings = $.extend({
+          x : 7,
+          y : 6,
+          player1 : 'Joueur 1',
+          player2 : 'Joueur 2',
+          colorp1 : 'red',
+          colorp2 : 'yellow'
+      }, options);
 
-let array = [];
-let turn = 0;
+      let array = [];
+      let turn = 0;
+      let scorep1 = 0;
+      let scorep2 = 0;
 
-function createGrid2(x, y){
-  $('body').prepend('<div>');
-  $('div').attr('id', 'grid');
-  $("#grid").append('<table>');
-  for (let rows = 0; rows < y; rows++) {
-    array[rows] = [];
-    let tr = document.createElement('tr');
-    for (let columns = 0; columns < x; columns++) {
-      let td = document.createElement('td');
-      td.setAttribute('id', rows+'-'+columns);
-      tr.append(td);
-      array[rows][columns] = 0;
-    }
-    $('table').append(tr); 
-  }
-  //for (let columns = 0; columns < x; columns++) {
-    //$("tr").append("<td id='"+rows+"-"+columns+"'>");
-  //}
-  $('body').prepend('<div id="bar">');
-  $('#bar').css({'color':'blue', 'text-align':'center'});
-}
-
-
-function getClickPosition(){
-
-  $("td").click(function(){
-    let column = $(this).index();
-    let len = array.length - 1;
-    let row;
-    turn += 1;
-    for (row = len; row > -1; row--) {
-      if (array[row][column] == 0) {
-        if(turn%2 == 1){
-        array[row][column] = 1;
-        $(`#${row}-${column}`).addClass('player1');
+      function createGrid(x, y){
+        if (settings.colorp1 == settings.colorp2){
+          alert('Mettez deux couleurs différentes dans vos options pour pouvoir jouer!');
+          return false;
         }
-        if(turn%2 == 0){
-          array[row][column] = 2;
-          $(`#${row}-${column}`).addClass('player2');
+        $('body').prepend('<div>');
+        $('div').attr('id', 'grid');
+        $("#grid").append('<table>');
+
+        for (let rows = 0; rows < y; rows++) {
+          array[rows] = [];
+          //let tr = document.createElement('tr');
+          $('table').append('<tr>');
+          for (let columns = 0; columns < x; columns++) {
+            //let td = document.createElement('td');
+            $('tr:last').append('<td id="'+rows+'-'+columns+'">');
+            //$('td').attr('id', rows+'-'+columns)
+            // td.setAttribute('id', rows+'-'+columns);
+            // tr.append(td);
+            array[rows][columns] = 0;
+          }
+          //$('table').append(tr); 
         }
-        break;
-      }
-    }
-    if(row < 0){
-      alert('Cette colonne est pleine!');
-    }
-    let joueur = $(`#${row}-${column}`).attr('class');
 
-    if(joueur == 'player1'){
-      $('#bar').text('Tour de : player2');
-    }
-    if(joueur == 'player2'){
-      $('#bar').text('Tour de : player1');
-    }
-    let player = array[row][column];
-    let count = 0;
-    let win = 0;
-    let maxRow = array.length;
-    let maxCol = array[row].length;
+        $('body').prepend('<div id="bar">');
+        $('#bar').css({'color':'grey', 'display':'flex', 'justify-content':'center', 'font-family':'"Comic Sans MS", cursive, sans-serif'});
+        $('table').css({'position': 'relative', 'font-size': 0, 'margin':'auto', 'padding': '5px', 'background-color': 'grey'});
+        $('td').css({'margin':0, 'padding':0, 'outline':'1px solid grey', 'border-radius': '50%', 'background-color': 'white', 'cursor': 'pointer', 'width': '75px', 'height': '75px'});
+        $('#bar').append('<p>');
+        $('p').attr('id', 'tours');
+        $('#bar').append('<button>');
+        $('button').attr('id', 'cancel');
+        $('button').css({'background-color':'transparent', 'cursor':'pointer', 'margin':'10px', 'border':'none','border':'1px solid grey', 'border-radius':'5px', 'padding':'10px', 'font-family':'"Comic Sans MS", cursive, sans-serif'});
+        $('#cancel').text('Annuler le dernier coup');
+        $('#cancel').hide();
+        $('#bar').append('<p id="scorep1">');
+        $('#bar').append('<p id="scorep2">');
+      };
 
-    //CHECK HORIZONTAL
-    for(i = 0; i < maxCol; i++){
-      if(array[row][i] == player){
-        count++;
-        if(count >= 4){
-          alert(player+' win ! END OF THE GAME');
-          $('td').removeClass();
-          $.each(array, function(row, column){
-            $.each(column, function(key, value){
-              column[key]= 0;
-            });
-          });
-        }
-      }
-      else{
-        count = 0;
-      }
-    }
-    count = 0;
-    //CHECK VERTICAL
-    for(i = 0; i < maxRow; i++){
-      if(array[i][column] == player){
-        count++;
-        if(count >= 4){
-          alert(player+' win ! END OF THE GAME');
-          $('td').removeClass();
-          $.each(array, function(row, column){
-            $.each(column, function(key, value){
-              column[key]= 0;
-            });
-          });
-          return 1;
-        }
-      }
-      else{
-        count = 0;
-      }
-    }
-
-
-    // //CHECK DIAGONALE BAS DROIT VERT HAUT GAUCHE OK
-    // for(i = row+1, j=column+1; i<maxRow && j<maxCol; i++, j++){
-    //   if(array[i][j] == player){
-    //     count++;
-    //     if(count >= 4){
-    //       alert(player+' win ! END OF THE GAME');
-    //       return 1;
-    //     }
-    //   }
-    //   else{
-    //     count = 0;
-    //   }
-    // }
-    // count = 0;
-
-    // for(i = row-1, j=column-1; i>= 0 && j>= 0; i--, j--){
-    //   if(array[i][j] == player){
-    //     count++;
-    //     if(count >= 4){
-    //       alert(player+' win ! END OF THE GAME');
-    //       return 1;
-    //     }
-    //   }
-    //   else{
-    //     count = 0;
-    //   }
-    // }
-
-
-    //DIAGONAL BAS GAUCHE VERS HAUT DROIT
-    // for(i= column-1, j= row-1; i>=0, j>=0; i--, j--){
-    //   if(array[j][i] == player){
-    //     count++;
-    //     if(count >= 4){
-    //       alert(player+' win ! END OF THE GAME');
-    //       return 1;
-    //     }
-    //   }
-    //   else{
-    //     count = 0;
-    //   }
-    // }
-    // let rowIndex = $(this).parent().index();
-    // let columnIndex = $(this).index();
-    // console.log(array);
-    // for(let i = columnIndex; i >= 0; i--){
-      //   console.log(array[rowIndex][i]);
-      //   if(array[rowIndex][i] == 0){
-           //if(!$(this).hasClass("player1") && !$(this).hasClass("player2")){
-          //       turn += 1;
-          //       if(turn%2 == 0){
-            //         array[rowIndex][i] = 1;
-            //         $(this).addClass('player1');
-            //       };
-            //       if(turn%2 == 1){
-              //         array[rowIndex][i] = 2;
-              //         $(this).addClass('player2');
-              //       };
-              //     };
-              //   }
+      function play(){
+        $("td").click(function(){
+          $('#cancel').show();
+          let column = $(this).index();
+          let len = array.length - 1;
+          let row;
+          turn += 1;
+          for (row = len; row > -1; row--) {
+            $('.played').removeClass('played');
+            if (array[row][column] == 0) {
+              $(`#${row}-${column}`).addClass('played');
+              if(turn%2 == 1){
+                array[row][column] = 1;
+                $(`#${row}-${column}`).css({'background-color': settings.colorp1});
+                $('#tours').text('Tour de : '+settings.player2);
+                //$('#tours').css('color', settings.colorp2);
+              }
+              if(turn%2 == 0){
+                array[row][column] = 2;
+                $(`#${row}-${column}`).css('background-color', settings.colorp2);
+                $('#tours').text('Tour de : '+settings.player1);
+                //$('#tours').css({'color': settings.colorp1});
+              }
+              break;
+            }
+          }
+          //let lenCol = array[row].length;
+          if(row < 0){
+            alert('Cette colonne est pleine!');
+          }
+          // if(column == lenCol -1){
+          //   alert('Partie nulle');
+          // }
+            // for (row = 0; row < len; row++) {
               // }
-    });
-};
+              
+          $('#cancel').click(function(){
+            if($(`#${row}-${column}`).hasClass('played')){
+              $('#tours').text('Tour de : '+settings['player' + array[row][column]]);
+              turn--;
+              array[row][column] = 0;
+              $(`#${row}-${column}`).css('background-color', 'white');
+            }
+          });
+          checkWin(array, row, column);
+        });
+      };
+        
 
-$(document).ready(function(){
-  createGrid2(7, 6);
-  getClickPosition();
-});
+      function checkWin(array, row, column){
+        let player = array[row][column];
+        let count = 0;
+        let maxRow = array.length;
+        let maxCol = array[row].length;
+        //testNul();
+        //CHECK HORIZONTAL
+        for(i = 0; i < maxCol; i++){
+          if(array[row][i] == player){
+            count++;
+            if(count >= 4){
+              alert(settings['player' + player] +" a gagné");
+              resetGrid();
+              if(player == 1){
+                scorep1++;
+                $('#scorep1').text('Score de '+settings.player1+': '+scorep1);
+              }
+              if(player == 2){
+                scorep2++;
+                $('#scorep2').text('Score de '+settings.player2+': '+scorep2);
+              }
+            }
+          }
+          else{
+            count = 0;
+          }
+        }
+        //CHECK VERTICAL
+        for(i = 0; i < maxRow; i++){
+          if(array[i][column] == player){
+            count++;
+            if(count >= 4){
+              alert(settings['player' + player] +" a gagné");
+              resetGrid();
+              if(player == 1){
+                scorep1++;
+                $('#scorep1').text('Score de '+settings.player1+': '+scorep1);
+              }
+              if(player == 2){
+                scorep2++;
+                $('#scorep2').text('Score de '+settings.player2+': '+scorep2);
+              }
+            }
+          }
+          else{
+            count = 0;
+          }
+        }
+        //BAS GAUCHE VERS HAUT DROIT OK
+        for (i=3; i<maxRow; i++){
+          for (j=0; j<maxCol-3; j++){
+            if (array[i][j] == player && array[i-1][j+1] == player && array[i-2][j+2] == player && array[i-3][j+3] == player){
+              alert(settings['player' + player] +" a gagné !");            
+              resetGrid();
+              if(player == 1){
+                scorep1++;
+                $('#scorep1').text('Score de '+settings.player1+': '+scorep1);
+              }
+              if(player == 2){
+                scorep2++;
+                $('#scorep2').text('Score de '+settings.player2+': '+scorep2);
+              }
+            }
+          }
+        }
+        //BAS DROIT VERS HAUT GAUCHE OK
+        for (i=3; i<maxRow; i++){
+          for (j=3; j<maxCol; j++){
+            if (array[i][j] == player && array[i-1][j-1] == player && array[i-2][j-2] == player && array[i-3][j-3] == player){
+              alert(settings['player' + player] +" a gagné !");
+              resetGrid();
+              if(player == 1){
+                scorep1++;
+                $('#scorep1').text('Score de '+settings.player1+': '+scorep1);
+              }
+              if(player == 2){
+                scorep2++;
+                $('#scorep2').text('Score de '+settings.player2+': '+scorep2);
+              }
+            }
+          }
+        }
+      };
+      
+      function resetGrid(){
+        $('td').css('background-color', 'white');
+        $.each(array, function(row, column){
+          $.each(column, function(key, value){
+            column[key]= 0;
+          });
+        });
+      };
+
+      function testNul(){
+        // $.each(array, function(row, column){
+        //   $.each(column, function(key, value){
+        //     if(column[key] != 0){
+        //       alert('Partie nulle!');
+        //     }
+        //   });
+        // });
+      //   let maxRow = array.length;
+      //   let maxCol = array[row].length;
+      //   count = 0;
+      //   for(i=0; i < maxCol; i++){
+      //     if(array[i][j] != 0){
+      //       count ++;
+      //     }
+      //     if(count == maxRow -1){
+      //       return true;
+      //     }
+      //     else{
+      //       return false;
+      //     }
+      //   } 
+      // };
+      // count =0;
+      // for(i=0; i <maxCol; i++){
+      //   if(testNul(i) == false){
+      //     break;
+      //   }
+      //   if(i == maxCol -1){
+      //     alert('Partie Nulle');
+      //   }
+      }
+
+  $(document).ready(function(){
+    createGrid(settings.x, settings.y);
+    play();
+  });
+};
+}(jQuery));
+$('body').power4({x: 6, y: 6, player1 : "Marine", player2: 'Lauriane', colorp1: 'green', colorp2: 'pink'});
